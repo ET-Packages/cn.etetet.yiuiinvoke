7f7f0907338c55382a322c4e8cbb71f99dc8a84e
Lsy <379338943@qq.com>
2024-08-03 19:53:13 +0800

    初始化

---
+++ Scripts/Core/Attribute/YIUIInvokeSystemOfAttribute.cs
@@ -0,0 +1,15 @@
+﻿/*using System;
+
+namespace ET
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class YIUIInvokeSystemOfAttribute : EnableClassAttribute
+    {
+        public Type type;
+
+        public YIUIInvokeSystemOfAttribute(Type type, bool ignoreAwake = false)
+        {
+            this.type = type;
+        }
+    }
+}*/

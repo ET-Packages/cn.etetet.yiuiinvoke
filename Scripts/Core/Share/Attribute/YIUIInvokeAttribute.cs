c84a097f699bc397e3df0001010bb76ffd1867eb
Lsy <379338943@qq.com>
2024-10-26 01:09:15 +0800

    重构Invoke Const

--- Scripts/Core/Share/Attribute/YIUIInvokeAttribute.cs
+++ Scripts/Core/Share/Attribute/YIUIInvokeAttribute.cs
@@ -2,8 +2,21 @@
 
 namespace ET
 {
+    //文档:https://lib9kmxvq7k.feishu.cn/wiki/TpyYwbWIUizhfKkcubocTZgInse
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class YIUIInvokeAttribute : BaseAttribute
     {
+        public string Type { get; }
+
+        //如果有参数必须是Const
+        public YIUIInvokeAttribute(string type)
+        {
+            this.Type = type;
+        }
+
+        //没有参数则SG读取的是方法名
+        public YIUIInvokeAttribute()
+        {
+        }
     }
 }
